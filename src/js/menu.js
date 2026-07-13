c6873f544853d70b9a25476c2314fb29aa0ddd99
konorezzova-spec <konorezzova@gmail.com>
2026-07-13 11:21:04 +0300

    the site is done

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,6 +1,7 @@
 const burgerMenu = document.querySelector('.burger-menu');
 const mobileMenu = document.querySelector('.nav-modal-overlay');
 const closeMobileMenuButton = document.querySelector('.close-modal');
+const mobileMenuLinks = document.querySelectorAll('.nav-modal-list-item, .modal-order-btn');
 
 
 burgerMenu.addEventListener('click', function () {
@@ -9,3 +10,7 @@ burgerMenu.addEventListener('click', function () {
 closeMobileMenuButton.addEventListener('click', function () {
     mobileMenu.classList.remove('is-open');
 })
+
+mobileMenuLinks.forEach((link) => link.addEventListener('click', function() {
+  mobileMenu.classList.remove('is-open');
+}))
